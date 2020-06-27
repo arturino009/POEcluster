5e24c2924ccf8d51b16bb21f560208f0825bbcd7
BuildTools <unconfigured@null.spigotmc.org>
2020-06-27 20:04:08 +0300

    Rewrite of the program. Now uses downloaded files from poecraft.com . Also able to index single notables, both small and medium cluster jewels.

--- apiMaybe/apiMaybe.py
+++ apiMaybe/apiMaybe.py
@@ -6,48 +6,102 @@ import statistics                   #so i get averages and medians of lists
 from tkinter import *               #so I can make fancy interface
 from TkTreectrl import *            #so I can make even fancier multiListBoxes  (manual installation required)
 import webbrowser                   #so i can open links in browser
-import list_categories              #other file with a lot of manually written code
+from bs4 import BeautifulSoup
+from requests_html import HTMLSession
+import os
+import json
+import ctypes
+import codecs
+
+print('Input 1 for single notable prices, 2 for double notable prices.') 
+query = int(input())
+inp = 0
+if query == 1:
+    print('Input 1 to check small cluster jewels, 2 for medium cluster jewels.') 
+    inp = int(input())
+    location = "/data/small/" if inp == 1 else "/data/medium/"
+else:
+    location = "/data/medium/"
+
+all_lists = list()
+
+for filename in os.listdir(os.getcwd() + location):
+    with open(os.getcwd() + location + filename) as data_file:
+        contents = data_file.read()
+        soup = BeautifulSoup(contents, 'lxml')
+        results = soup.findAll("div", {"modgrp" : "unique_notable"} if inp == 1 else {"modgrp" : re.compile("Notable")})
+        nameofcategory = soup.findAll("div", {"class": "choice med_shadow"})
+        weightofaffix = soup.findAll("div", {"class": "tpct"})
+        a = list() 
+        for g in results:
+            with open('stats.json') as json_file:
+                data = json.load(json_file)
+                for i in data['result'][1]['entries']:
+                    if i['text'] == g.contents[0].text:
+                        b = {
+                            'id': i['id'],
+                            'name': g.contents[0].text,
+                            'prefix': float(g.contents[4].text[:-1]),
+                            'weight': float(g.contents[5].text[:-1])
+                            }
+                        a.append(b)
+                        print(g.contents[0].text)
+                        continue
+        if "@" in filename:
+            filename = filename.replace("@", '\\')[:-5]
+        else:
+            filename = filename[:-5]
+        with open('stats.json') as json_file:
+                data = json.load(json_file)
+                for i in data['result'][4]['entries'][0]['option']['options']:
+                    if i['text'] == codecs.decode(filename, 'unicode_escape'):
+                        id = i['id']
+                        break
+        c = {
+            'id': id,
+            'category': filename,
+            'notables': a
+            }
+        all_lists.append(c)
+
 
 #start the timer for program execution
 start_time = time.time()
-   
-#import all lists from list_categories.py file
-lists = list_categories.make_lists()
 
-#wasting_aff = 'explicit.stat_2066820199'
-#vivid_hues = 'explicit.stat_3957006524'
-current_exa_price = 137                                             #current exa price https://poe.ninja/challenge/currency/exalted-orb
-
+session = HTMLSession()
+resp = session.get("https://poe.ninja/challenge/currency/exalted-orb")
+resp.html.render()
+soup = BeautifulSoup(resp.html.html, "lxml")
+res = soup.find_all("span", class_="currency-amount")
+current_exa_price = round(float(res[0].contents[0]))                                             #current exa price https://poe.ninja/challenge/currency/exalted-orb
 
 #list of all values that I will get
 all_averages = list()
-
-#clear the files
-open('output.txt', 'w').close()
-open('expensive.txt', 'w').close()
-
-for a in lists:
-    #make a list of all possible combinations of items in each category
-    comb = list(combinations(a['list'],2))
+all_averages.clear()
+for a in all_lists:
+    if query == 1:
+        comb = a['notables']
+    else:
+        #make a list of all possible combinations of items in each category
+        comb = list(combinations(a['notables'],2))
     for b in comb:
         data_set = {        #structure for API request. All info from https://www.reddit.com/r/pathofexiledev/comments/7aiil7/how_to_make_your_own_queries_against_the_official/ . Absolutely no other documentation
             "query": {
                 "status": {
                     "option": "online"
                 },
-                "type": "Medium Cluster Jewel",
+                "type": "Small Cluster Jewel" if inp == 1 else "Medium Cluster Jewel",
                 "stats": [{
                     "type": "and",
-                                   #value1                 #value2                  #category
-                    "filters": [{"id":b[0]['id']['id']}, {"id":b[1]['id']['id']}, {"id":'enchant.stat_3948993189',
-                                                                                   "value":{"option":a['id']}
-                                                                                   }]
+                                    #value1                   #category
+                    #"filters": [{"id":'enchant.stat_3948993189', "value":{"option":a['id']}}, {"id":b['id']}] if query == 1 else [{"id":'enchant.stat_3948993189', "value":{"option":a['id']}}, {"id":b[0]['id']}, {"id":b[1]['id']}]
+                    "filters": [{"id":b['id']}] if query == 1 else [{"id":b[0]['id']}, {"id":b[1]['id']}]
                 }],
                 "filters": {
                     "type_filters": {
                         "filters": {
                             "rarity": {
-						        "option": "rare"
+						        "option": "nonunique"
 					            }
                             }
                         }
@@ -57,18 +111,19 @@ for a in lists:
                 "price": "asc"
             }
         }
-        #time delay, so API wont rate limit me
-        time.sleep(0.4)
-        
         #send the request to API
+        print("Sending request...")
         response = requests.post('https://www.pathofexile.com/api/trade/search/Harvest', json=data_set)
         response = response.json()
+        print("Got response!")
+
         result = response['result']
         id = response['id']
         size = response['total']
 
         #if there are less than 10 listings for an item, we just just skip it (no demand)
-        if size < 10:
+        if size < 5:
+            print("Not enough items! Skipping...")
             continue    
 
         #if there are more than 10 listings, strip all of them away after 10th. We cant request info about items more than 10 items at once
@@ -81,18 +136,24 @@ for a in lists:
         else:
             str1 = result
 
+        #time delay, so API wont rate limit me
+        time.sleep(0.4)
         #get all actual listings of items
+        print("Requesting item info...")
         address = 'https://www.pathofexile.com/api/trade/fetch/' + str(str1) + '?query=' + id
         request = requests.get(address)
         results_json = request.json()
         
         #probability to get an item while crafting. Formula is mostly correct
-        probability = b[0]['percent'] * b[1]['percent'] / 19.2
+        if query == 1:
+            probability = b['prefix']
+            cost_of_try = 0.05
+        else:
+            probability = b[0]['weight'] * b[1]['weight'] / 19.2
+            cost_of_try = 0.36
+
         #get number of tries to get item
         tries = 100 / probability   
-
-        #approximate cost of 1 try
-        cost_of_try = 0.36
 
         #price to create an item (approximate)
         price = tries * cost_of_try
@@ -100,72 +161,65 @@ for a in lists:
         #list to hold all prices of an item. Later used to calculate medium price
         medium = list()
 
-        f = open("output.txt", "a")
-        expensive = open("expensive.txt", "a")
-
-        f.write(b[0]['id']['text'] + " and " + b[1]['id']['text'] + ": " + str(round(probability,3)) + "%" + " Cost for rerolls: " + str(round(price,2)) + " Tries: " + str(round(tries)) + '\n')
-        f.write('Listings:' + str(size) + '\n')
-        print(b[0]['id']['text'] + " and " + b[1]['id']['text'] + ": " +  str(round(probability,3))  + "%" + " Cost for rerolls: " + str(round(price,2))+ " Tries: " + str(round(tries)))
-        print('Listings:' + str(size))
+        if query == 1:
+            print(b['name'] + ": " +  str(round(probability,3))  + "%" + " Cost for rerolls: " + str(round(price,2))+ " Tries: " + str(round(tries)))
+            print('Listings:' + str(size))
+        else:
+            print(b[0]['name'] + " and " + b[1]['name'] + ": " +  str(round(probability,3))  + "%" + " Cost for rerolls: " + str(round(price,2))+ " Tries: " + str(round(tries)))
+            print('Listings:' + str(size))
         
         #used to calculate actual count of items with prices
         count = 0
         for p in results_json['result']:
             #if item has no price listed, set as No price
             if(p['listing']['price'] != None):
-                count = count + 1   
                 #conversion for some more valuable currency
                 if(p['listing']['price']['currency'] == "exalted"):
                     p['listing']['price']['amount'] = p['listing']['price']['amount'] * current_exa_price
                     p['listing']['price']['currency'] = "chaos"
                 if(p['listing']['price']['currency'] == "chaos"):
+                    count = count + 1 
                     medium.append(p['listing']['price']['amount'])
                 print("Price: ", p['listing']['price']['amount'], " " , p['listing']['price']['currency'], '\n')
-                f.write("Price: " + str(p['listing']['price']['amount']) + " " + p['listing']['price']['currency'] + '\n')
             else:
-                print("No price")   
-        #get the average median of all listed prices for an item
-        avg = statistics.median_grouped(medium)
-        #profit margin 
-        profit = avg - price
-        print("The average median is ", round(avg,2), "     Profit:", round(profit,2), '\n')
-        f.write("The average median is                             " + str(round(avg,2)) + " Profit:          " + str(round(profit,2)) + '\n')
+                print("No price") 
         #if less than 10 listings have prices, skip the item
-        if count == 10:
+        if count > 4:
+            #get the average median of all listed prices for an item
+            avg = statistics.median_grouped(medium)
+            #profit margin 
+            profit = avg - price - 1
+            first = (medium[0]+medium[1])/2
+            LPPT = (first - price - 1)/tries
+            print("The average median is ", round(avg,2), "     Profit:", round(profit,2), '\n')
             x = {
-                'name': b[0]['id']['text'] + " and " + b[1]['id']['text'],
+                'name': b['name'] if query == 1 else (b[0]['name'] + " and " + b[1]['name']),
                 'listings': size,
                 'tries': round(tries),
+                'first': round(first,2),
                 'average': round(avg,2),
                 'profit': round(profit,2),
                 'PPT': round(profit/tries,3),
+                'LPPT': round(LPPT,3),    #when first item is a lot cheaper than average
                 'category': a['category'],
                 'id': id
                 }
             all_averages.append(x)
-            #if there is profit to be made, print to different file
-            if x['profit'] > 0:
-                expensive.write(str(x) + '\n')
-                expensive.write('Profit per try:'+ str(round(profit/tries,3)) + '\n')
         medium.clear()
-        f.close()
-        expensive.close()    
-
-
 #lists made for sorting
 sorted_list_count = list()
 sorted_list_tries = list()
 sorted_list_price = list()
 sorted_list_profit = list()
 sorted_list_ppt = list()
+sorted_list_lppt = list()
 
 sorted_list_count = sorted(all_averages, key = lambda i: i['listings'],reverse=True)
 sorted_list_tries = sorted(all_averages, key = lambda i: i['tries'],reverse=True)
 sorted_list_price = sorted(all_averages, key = lambda i: i['average'],reverse=True)
 sorted_list_profit = sorted(all_averages, key = lambda i: i['profit'],reverse=True)
 sorted_list_ppt = sorted(all_averages, key = lambda i: i['PPT'],reverse=True)
-
-
+sorted_list_lppt = sorted(all_averages, key = lambda i: i['LPPT'],reverse=True)
 #need to keep track of currently displayed list
 current_sort = list()
 current_sort = all_averages
@@ -173,22 +227,21 @@ current_sort = all_averages
 def open_link(q):
     webbrowser.open('https://www.pathofexile.com/trade/search/Harvest/' + current_sort[q]['id'])
 
-
 #section for creating Tkinter window
 root = Tk()
 root.title("Results")
 root.geometry("1200x800")
-for i in range(0, 6):
+for i in range(0, 7):
      root.columnconfigure(i, weight=1)
 root.rowconfigure(0, weight=1)
 
 streeview = ScrolledMultiListbox(root, scrollmode = 'auto')
-streeview.grid(columnspan=6, row=0, padx=5,pady=5,sticky='nsew')
+streeview.grid(columnspan=7, row=0, padx=5,pady=5,sticky='nsew')
 treeview = streeview.listbox
 treeview.focus_set()
-treeview.config(columns=('Name', 'Listings','Tries','Average price','Profit','PPT', 'Category'), command=open_link)
+treeview.config(columns=('Name', 'Listings','Tries','First','AVG price','Profit','PPT', 'LPPT', 'Category'), command=open_link)
 for item in all_averages:
-    treeview.insert(END,item['name'],item['listings'],item['tries'],item['average'],item['profit'],item['PPT'], item['category'])
+    treeview.insert(END,item['name'],item['listings'],item['tries'],item['first'],item['average'],item['profit'],item['PPT'], item['LPPT'], item['category'])
 
 #replacing the listbox with requested sort
 def sort_items(given_list):
@@ -196,7 +249,7 @@ def sort_items(given_list):
     global current_sort
     current_sort = given_list
     for item in given_list:
-        treeview.insert(END,item['name'],item['listings'],item['tries'],item['average'],item['profit'],item['PPT'], item['category'])
+        treeview.insert(END,item['name'],item['listings'],item['tries'],item['first'],item['average'],item['profit'],item['PPT'], item['LPPT'], item['category'])
 
 result_time = round(time.time() - start_time, 3)
 time_label = Label(root, text="Executed in " + str(result_time) + " seconds").grid(column=0)
@@ -207,6 +260,14 @@ button3 = Button(root,text="Sort by tries", command=lambda:sort_items(sorted_lis
 button4 = Button(root,text="Sort by price", command=lambda:sort_items(sorted_list_price)).grid(column=3, row=1)
 button5 = Button(root,text="Sort by profit", command=lambda:sort_items(sorted_list_profit)).grid(column=4, row=1)
 button6 = Button(root,text="Sort by PPT", command=lambda:sort_items(sorted_list_ppt)).grid(column=5, row=1)
+button7 = Button(root,text="Sort by LPPT", command=lambda:sort_items(sorted_list_lppt)).grid(column=6, row=1)
 
+#hiding the console
+kernel32 = ctypes.WinDLL('kernel32')
+user32 = ctypes.WinDLL('user32')
+SW_HIDE = 0
+hWnd = kernel32.GetConsoleWindow()
+if hWnd:
+    user32.ShowWindow(hWnd, SW_HIDE)
 
 root.mainloop()
