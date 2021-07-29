c0e29227ed9069f89e3f0a2bbcc9ddb431259984
arturino009 <arturino009@inbox.lv>
2021-07-30 00:41:58 +0300

    Fix for 3.15

--- updateClusterData.py
+++ updateClusterData.py
@@ -51,7 +51,7 @@ def get_data_poedb(size):
         else:
             weightOfNotables = weightOfNotables + 8000      #~14000
 
-        for i in allStats['result'][4]['entries'][3]['option']['options']:
+        for i in allStats['result'][4]['entries'][1]['option']['options']:
             if i['text'] == nameOfCluster:
                 clusterId = i['id']
                 break
